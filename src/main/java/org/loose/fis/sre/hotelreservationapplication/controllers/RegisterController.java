ec6c783450b4d26e1eb2efa41481cd7f83c989b7
Novacovici Sonia <sonianovacovici@gmail.com>
2022-04-25 22:00:08 +0300

    added password encoding to HRA-17.HRA-23

--- src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/RegisterController.java
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/RegisterController.java
@@ -32,10 +32,10 @@ public class RegisterController {
     @FXML
     public void handleRegisterAction() {
         try {
-            if (usernameField.getText() == "" || passwordField.getText() == "" || (String) role.getValue() == "" || fullNameField.getText() == "" || phoneNumberField.getText() == "") {
+            if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty() || (String) role.getValue() == null || fullNameField.getText().isEmpty() || phoneNumberField.getText().isEmpty()) {
                 registrationMessage.setText("Please fill in all the fields!");
             } else {
-                UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue(), fullNameField.getText(), phoneNumberField.getText());
+                UserService.addUser(usernameField.getText(), UserService.encodePassword(passwordField.getText()), (String) role.getValue(), fullNameField.getText(), phoneNumberField.getText());
                 registrationMessage.setText("Account created successfully!");
             }
         } catch (SQLException e) {
--- src/main/java/org/loose/fis/sre/hotelreservationapplication/services/UserService.java
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/services/UserService.java
@@ -2,6 +2,8 @@ package org.loose.fis.sre.hotelreservationapplication.services;
 
 import org.loose.fis.sre.hotelreservationapplication.database.DBConnection;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,7 +12,24 @@ public class UserService {
     public static void addUser(String username, String password, String role, String fullName, String phoneNumber) throws SQLException {
 
         Statement statement = DBConnection.connection.createStatement();
-        statement.executeUpdate("INSERT INTO user (username, password, role, fullName, phoneNumber) VALUES ('" + username + "', '" + password + "', '" + role + "', '" + fullName + "', '" + phoneNumber + "');");
+        statement.executeUpdate("INSERT INTO users (username, password, role, fullName, phoneNumber) VALUES ('" + username + "', '" + password + "', '" + role + "', '" + fullName + "', '" + phoneNumber + "');");
+    }
+
+    public static String encodePassword (String password) {
+        String encryptedpassword = null;
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(password.getBytes());
+            byte[] bytes = m.digest();
+            StringBuilder s = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++) {
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            encryptedpassword = s.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encryptedpassword;
     }
 
 }
