751ec20053ce4ec5e3f1dd85d7b93eeb96ef03eb
Novacovici Sonia <sonianovacovici@gmail.com>
2022-05-05 11:04:56 +0300

    added controllers to HRA-7

---
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/OneRoomController.java
@@ -0,0 +1,21 @@
+package org.loose.fis.sre.hotelreservationapplication.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class OneRoomController {
+
+    @FXML
+    private Label nrPers;
+
+    @FXML
+    private Label price;
+
+    @FXML
+    private ImageView roomImg;
+
+    @FXML
+    private Label type;
+
+}
---
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/RoomsController.java
@@ -0,0 +1,15 @@
+package org.loose.fis.sre.hotelreservationapplication.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+
+public class RoomsController {
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private ScrollPane scroll;
+
+}
