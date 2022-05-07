e4875e92fea53942b22d3999c205938502b17ffd
Ioana-Novacovici <ioananovacovici@gmail.com>
2022-05-07 17:03:01 +0300

    added Reservation services to HRA-11

---
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/services/ReservationService.java
@@ -0,0 +1,26 @@
+package org.loose.fis.sre.hotelreservationapplication.services;
+
+import org.loose.fis.sre.hotelreservationapplication.database.DBConnection;
+
+import java.sql.Date;
+
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ReservationService {
+
+    public static void addReservation(String username, Date startDate, Date endDate, String type, boolean extraBed, boolean breakfast, boolean parking) throws SQLException {
+
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("INSERT INTO reservations (username, startDate, endDate, type, extraBed, breakfast, parking) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        statement.setString(1, username);
+        statement.setDate(2, startDate);
+        statement.setDate(3, endDate);
+        statement.setString(4, type);
+        statement.setString(5, String.valueOf(extraBed));
+        statement.setString(6, String.valueOf(breakfast));
+        statement.setString(7, String.valueOf(parking));
+        statement.executeUpdate();
+    }
+}
