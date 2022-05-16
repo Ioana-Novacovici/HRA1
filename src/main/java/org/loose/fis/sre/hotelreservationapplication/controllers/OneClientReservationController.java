1af3cbcce27a53057cb6a8471416bb7dab319c92
Novacovici Sonia <sonianovacovici@gmail.com>
2022-05-17 01:38:39 +0300

    setData for one reservation for HRA-8

--- src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/OneClientReservationController.java
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/OneClientReservationController.java
@@ -1,4 +1,55 @@
 package org.loose.fis.sre.hotelreservationapplication.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import org.loose.fis.sre.hotelreservationapplication.models.Reservation;
+
 public class OneClientReservationController {
+
+    @FXML
+    private Label breakfastOption;
+
+    @FXML
+    private Label checkIn;
+
+    @FXML
+    private Label checkOut;
+
+    @FXML
+    private Label extraBedOption;
+
+    @FXML
+    private Label parkingOption;
+
+    @FXML
+    private Label totalPrice;
+
+    @FXML
+    private Label typeOfRoom;
+
+    public void setData(Reservation reservation) {
+        if(reservation.isBreakfast()) {
+            breakfastOption.setTextFill(Color.valueOf("#14430c"));
+        }
+        else {
+            breakfastOption.setTextFill(Color.valueOf("#c10101"));
+        }
+        if(reservation.isExtraBed()) {
+            extraBedOption.setTextFill(Color.valueOf("#14430c"));
+        }
+        else {
+            extraBedOption.setTextFill(Color.valueOf("#c10101"));
+        }
+        if(reservation.isParking()) {
+            parkingOption.setTextFill(Color.valueOf("#14430c"));
+        }
+        else {
+            parkingOption.setTextFill(Color.valueOf("#c10101"));
+        }
+        checkIn.setText(String.valueOf(reservation.getStartDate()));
+        checkOut.setText(String.valueOf(reservation.getEndDate()));
+        totalPrice.setText(String.valueOf(reservation.getPrice()));
+        typeOfRoom.setText(reservation.getType());
+    }
 }
