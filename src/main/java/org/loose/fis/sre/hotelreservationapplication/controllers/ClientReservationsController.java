55b3c28603428b5adeaf7a2aa38a5be6543e01ad
Novacovici Sonia <sonianovacovici@gmail.com>
2022-05-17 00:25:42 +0300

    base structure for my reservations window for HRA-8

--- src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/ClientReservationsController.java
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/ClientReservationsController.java
@@ -1,4 +1,21 @@
 package org.loose.fis.sre.hotelreservationapplication.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import org.loose.fis.sre.hotelreservationapplication.Main;
+
 public class ClientReservationsController {
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    public void goBack(){
+        Main.changeToScene("rooms.fxml");
+    }
+
 }
