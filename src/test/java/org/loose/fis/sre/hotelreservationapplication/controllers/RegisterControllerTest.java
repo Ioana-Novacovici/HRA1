cdecb3534d4e31b1c138753e3c8e0424463c7224
Novacovici Sonia <sonianovacovici@gmail.com>
2022-05-20 17:12:49 +0300

    room service testing for HRA-53

--- src/test/java/org/loose/fis/sre/hotelreservationapplication/controllers/RegisterControllerTest.java
+++ src/test/java/org/loose/fis/sre/hotelreservationapplication/controllers/RegisterControllerTest.java
@@ -1,13 +1,11 @@
 package org.loose.fis.sre.hotelreservationapplication.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegisterControllerTest {
 
-    @Test
-    public void testBool(){
-        assertEquals(4, 2 + 2);
-    };
+
 }
