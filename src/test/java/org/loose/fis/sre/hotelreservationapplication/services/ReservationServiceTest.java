4b4ae6b9055a73b8fb0c3b16618f087a14efdcf5
Ioana-Novacovici <ioananovacovici@gmail.com>
2022-05-21 11:46:27 +0300

    reservation service testing done to HRA-53

--- src/test/java/org/loose/fis/sre/hotelreservationapplication/services/ReservationServiceTest.java
+++ src/test/java/org/loose/fis/sre/hotelreservationapplication/services/ReservationServiceTest.java
@@ -8,6 +8,7 @@ import org.loose.fis.sre.hotelreservationapplication.database.DBConnection;
 
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ReservationServiceTest {
@@ -132,8 +133,133 @@ public class ReservationServiceTest {
         statement.executeUpdate();
     }
 
+    @Test
+    void getFutureReservationsIDSTest() throws SQLException{
+        Date date1 = new Date(2035, 10, 12);
+        Date date2 = new Date(2035, 12, 12);
+        ReservationService.addReservation("ioana", date1, date2, "accepted", "Single Room", true, true, true, 500);
+        ReservationService.addReservation("ioana", date1, date2, "accepted", "Single Room", true, true, true, 500);
+        ArrayList <Integer> ids = ReservationService.getFutureReservationsID();
+        Assertions.assertEquals(ids.size(), 2);
+
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("DELETE from reservations");
+        statement.executeUpdate();
+    }
+
+    @Test
+    void getWaitingReservationsIDSTest() throws SQLException{
+        LocalDate date= LocalDate.now();
+        ReservationService.addReservation("ioana", Date.valueOf(date), Date.valueOf(date), "waiting", "Single Room", true, true, true, 500);
+        ResultSet reservation = ReservationService.getWaitingReservations();
+        ArrayList <Integer> ids = ReservationService.getWaitingReservationsID();
+        Assertions.assertEquals(ids.size(), 1);
+
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("DELETE from reservations");
+        statement.executeUpdate();
+    }
+
+    @Test
+    void getPastReservationsIDSTest() throws SQLException{
+        LocalDate date= LocalDate.now();
+        ReservationService.addReservation("ioana", Date.valueOf(date), Date.valueOf(date), "accepted", "Single Room", true, true, true, 500);
+        ArrayList <Integer> ids = ReservationService.getPastReservationsID();
+        Assertions.assertEquals(ids.size(), 0);
 
 
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("DELETE from reservations");
+        statement.executeUpdate();
+    }
 
+    @Test
+    void updateReservationTest() throws SQLException{
+        Date date1 = new Date(2035, 10, 12);
+        Date date2 = new Date(2035, 12, 12);
+        ReservationService.addReservation("ioana", date1, date2, "accepted", "Single Room", true, true, true, 500);
+        ResultSet reservation = ReservationService.getFutureReservations();
+        int id = 0;
+        if(reservation.next()){
+            id = reservation.getInt(1);
+        }
+        ReservationService.updateReservation(id, date1, date2, "Double Room", false, false, false, 100);
+        reservation = ReservationService.getFutureReservations();
+        if(reservation.next()){
+            Assertions.assertEquals(reservation.getInt(10), 100);
+            Assertions.assertEquals(reservation.getString(6), "Double Room");
+            Assertions.assertFalse(reservation.getBoolean(7));
+            Assertions.assertFalse(reservation.getBoolean(8));
+            Assertions.assertFalse(reservation.getBoolean(9));
+        }
 
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("DELETE from reservations");
+        statement.executeUpdate();
+
+    }
+
+    @Test
+    void acceptReservationTest() throws SQLException{
+        Date date1 = new Date(2035, 10, 12);
+        Date date2 = new Date(2035, 12, 12);
+        ReservationService.addReservation("ioana", date1, date2, "waiting", "Single Room", true, true, true, 500);
+        ResultSet reservation = ReservationService.getFutureReservations();
+        int id = 0;
+        if(reservation.next()){
+            id = reservation.getInt(1);
+        }
+        ReservationService.acceptReservation(id);
+        reservation = ReservationService.getFutureReservations();
+        if(reservation.next()){
+            Assertions.assertEquals(reservation.getString(5), "accepted");
+        }
+
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("DELETE from reservations");
+        statement.executeUpdate();
+
+    }
+
+    @Test
+    void rejectReservationTest() throws SQLException{
+        Date date1 = new Date(2035, 10, 12);
+        Date date2 = new Date(2035, 12, 12);
+        ReservationService.addReservation("ioana", date1, date2, "waiting", "Single Room", true, true, true, 500);
+        ResultSet reservation = ReservationService.getFutureReservations();
+        int id = 0;
+        if(reservation.next()){
+            id = reservation.getInt(1);
+        }
+        ReservationService.rejectReservation(id);
+        reservation = ReservationService.getFutureReservations();
+        if(reservation.next()){
+            Assertions.assertEquals(reservation.getString(5), "rejected");
+        }
+
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("DELETE from reservations");
+        statement.executeUpdate();
+    }
+
+    @Test
+    void deleteReservationTest() throws SQLException{
+        Date date1 = new Date(2035, 10, 12);
+        Date date2 = new Date(2035, 12, 12);
+        ReservationService.addReservation("ioana", date1, date2, "waiting", "Single Room", true, true, true, 500);
+        ResultSet reservation = ReservationService.getFutureReservations();
+        int id = 0;
+        if(reservation.next()){
+            id = reservation.getInt(1);
+        }
+        ReservationService.deleteReservation(id);
+        reservation = ReservationService.getFutureReservations();
+
+        Assertions.assertEquals(reservation.next(), false);
+
+        PreparedStatement statement;
+        statement = DBConnection.connection.prepareStatement("DELETE from reservations");
+        statement.executeUpdate();
+
+    }
 }
